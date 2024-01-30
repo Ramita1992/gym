8f47eb52d4a7ce66854079e1ea115ae6ffddd826
Ramita1992 <ramita.dy@gmail.com>
2024-01-30 19:28:15 +0530

    Gym Website

---
+++ js/script.js
@@ -0,0 +1,273 @@
+
+
+// document.write("Welcome");
+ // document.getElementById('abcd').innerHTML="welcomeRamita";
+// window.alert("");
+// alert("welcome");
+// console.log("welcome");
+
+
+// a = 5
+// b = 5
+// c = a/b
+
+// document.write (c)
+
+// ==
+// < , >
+// <=
+// >=
+
+
+
+// a = 10
+// b = 15
+
+// if (a == b) {
+
+//   alert(" a is big");
+
+// }
+
+// else if(a >= b){
+
+//   alert ("b")
+
+// }
+
+// else{
+
+//   alert (" welcome");
+
+// }
+
+
+
+// a = 20
+// b = 30
+// c = a+b
+// d = a*b
+
+// if (a == b) {
+
+//   document.write("<h1 style='color:red;'> c </h1> ")
+// }
+
+// else if (a < b) {
+
+//   document.write("<p style='color:green;'> d </p>")
+// }
+
+// else {
+
+//   document.write("Don't worry")
+// }
+
+
+// a = 20
+// b = 30
+// // c = a+b
+// // d = a*b
+
+// if(a==b) {
+
+//   alert (a+b)
+// }
+
+// else if (a<b) {
+
+//   alert (a*b)
+
+// }
+
+// else {
+
+//   alert ("don't worry")
+// }
+
+
+// nested loop
+// nested ifelse
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+
+
+
+
+// i = 1;
+
+// while(i <= 10){
+
+// document.write("<br>"+i); 
+
+// i++;
+
+// }
+
+
+document.write("<br>");
+
+i = 2;
+
+  for(j = 1; j <= 10; j++){
+
+    c = i*j;
+
+    d = c*i;
+
+    document.write(i+ "*" +j+ "="+ c+"*"+i+"="+ d+ "<br>");
+
+  }
+
+
+document.write("<hr>");
+// a = 5;
+// b = 1;
+// e = 6;
+// f = 20;
+
+// while(b <= 10){
+
+//   cc = a*b;
+
+//   dd = cc/e;
+
+//   ff = dd+f;
+
+//   document.write(a+"*"+b+ "=" + cc+ "/"+ e+ "="+ dd+ "+"+ f+ "="+ ff+ "<br>");
+
+//   b++;
+
+// }
+
+document.write("<hr>");
+
+// 99 * 1 = result - 99 = result + 99 = rsult - 99
+ 
+
+aa = 99;
+bb = 1;
+
+while(bb <= 10){
+
+cc = aa*bb;
+dd = cc-aa;
+ee = dd-aa;
+ff = ee-aa;
+
+document.write(aa+ "*"+ bb+ "="+ cc+ "-"+ aa+ "="+ dd+ "+"+ aa+ "="+ ee+ "/"+ aa+ "="+ ff+  "<br>");
+
+bb++;
+
+}
+
+
+
+<hr>
+<hr>
+
+
+// break
+// continue
+
+for( i = 1; i <= 5; i++ ){
+
+ 
+ if (i == 2) {
+
+  continue;
+
+ }
+
+
+ document.write(i);
+
+
+
+
+}
+
+document.write("<hr>");
+
+u = 0;
+
+while(u<=5){
+
+  document.write("<div id='demo' style='width: 100px;height: 100px;background-color: blue;'> </div>");
+  document.write("<br>");
+
+  if (u == 3) {
+    
+    break;
+
+  }
+
+  u++;
+
+  
+}
+
+
+
+<hr>
+
+<h1 id="text">Ramita</h1>
+<button onmouseover ="document.getElementById('text').style.color='red' ">Click Me</button>
+
+
+
+
+
+// for(i = 0; i <= 10; i++){
+
+// 	document.write(i);
+// 	document.write("<br>"+);
+// }
+
+
+i = 2;
+
+  for(j = 1; j <= 10; j++){
+
+    c = i*j;
+
+    d = c*i;
+
+    document.write(i+ "*" +j+ "="+ c+"*"+i+"="+ d+ "<br>");
+    // document.write("<br>");
+
+
+    // d = c*i;
+
+    // document.write(c+ "*" +i+ "="+ +d+ "<br");
+
+
+  }
+
+document.write("<hr>");
+// a = 5;
+// b = 1;
+// e = 6;
+// f = 20;
+
+// while(b <= 10){
+
+//   cc = a*b;
+
+//   dd = cc/e;
+
+//   ff = dd+f;
+
+//   document.write(a+"*"+b+ "=" + cc+ "/"+ e+ "="+ dd+ "+"+ f+ "="+ ff+ "<br>");
+
+//   b++;
+
+// }
+
+
+
+
+
+
+
